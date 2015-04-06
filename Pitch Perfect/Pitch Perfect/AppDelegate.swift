4e5695b58b9ea526e02dcb4f757b3dc880715f71
bwtnmjx <bwtnmjx@users.noreply.github.com>
2015-04-06 13:53:12 -0400

    Committed by Udacity

---
+++ Pitch Perfect/Pitch Perfect/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Pitch Perfect
+//
+//  Created by Naresh Chavda on 3/16/15.
+//  Copyright (c) 2015 Naresh Chavda. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+}
+
